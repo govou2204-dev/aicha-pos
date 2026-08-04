289f9d39ea8b46387d0c2a1813009e44d5e52e65
govou2204-dev <govou2204@gmail.com>
2026-08-04 01:10:19 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
-  return {
-    base: '/aicha-pos/',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR !== 'true' ? undefined : { ignored: ['**/*'] },
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/aicha-pos/',
+})
